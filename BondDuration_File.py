c5510fb7f9db5a6812f4b5e696ffc6ee3afa44d6
koko61gg <157545008+koko61gg@users.noreply.github.com>
2024-04-11 10:24:30 -0400

    BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,14 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    pvcfsum = 0
+    w_pvcf = 0
+    cf = face * couponRate
+    for t in range(1,m+1):
+        pv = (1+y)**(-t)
+        pvcf = cf * pv
+        pvcfsum += pvcf
+        w_pvcf = w_pvcf + pvcf*t
+    bondPrice = pvcfsum + face * pv
+    pvcf_t = w_pvcf + face * pv *t
+    bondDuration = pvcf_t/bondPrice
+    return(bondDuration)
