7fc20c09c88d6f93f311f13c553ffe169ce4290d
koko61gg <157545008+koko61gg@users.noreply.github.com>
2024-04-11 10:25:43 -0400

    BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,12 @@
 
-
 def getBondPrice_E(face, couponRate, m, yc):
-    return(2098949)
+    bondPrice = 0
+    for t, y in enumerate(yc, 1):
+        cf = face * couponRate
+        pv = (1 + y) ** (-t)
+        pvcf = cf * pv
+        if t < m:
+            bondPrice += pvcf
+        else:
+            bondPrice += pvcf + face * pv
+    return(bondPrice)
